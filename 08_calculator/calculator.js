3249e8f01f577b4ba56c4abb3b26e65958853f21
Ramon Nario <rtnario@yahoo.com.ph>
2022-05-12 01:24:07 +0800

    Finished calculator and palindrome exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+  let sum = 0;
+
+  for (number of numbers) {
+    sum += number;
+  }
+
+  return sum;
 };
 
-const multiply = function() {
+const multiply = function(numbers) {
+  let product = numbers[0];
 
+  for (let i = 0; i < numbers.length - 1; i++) {
+    product *= numbers[i + 1];
+  }
+
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num === 0 || num === 1) return 1;
+
+  let fact = 1;
+
+  for (let i = 1; i < num; i++) {
+    fact *= i + 1;
+  }
+
+  return fact;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,27 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const processedString = string.toLowerCase().replaceAll(/\W/g,"");
 
+    let pivotCharIndex = 0;
+    let firstHalf = "";
+    let secondHalf = "";   
+
+    if (processedString.length % 2 !== 0) {
+        pivotCharIndex = (processedString.length - 1) / 2;
+        firstHalf = processedString.slice(0, pivotCharIndex);
+        secondHalf = processedString.slice(pivotCharIndex + 1, processedString.length)
+                                    .split("")
+                                    .reverse()
+                                    .join("");
+    } else {
+        pivotCharIndex = (processedString.length) / 2;
+        firstHalf = processedString.slice(0, pivotCharIndex);
+        secondHalf = processedString.slice(pivotCharIndex, processedString.length)
+                                    .split("")
+                                    .reverse()
+                                    .join("");
+    }
+
+    return firstHalf === secondHalf;
 };
 
 // Do not edit below this line

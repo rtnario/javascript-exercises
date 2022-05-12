f45dfa3d0f8a9ca62164ff3055afaeff5bb59350
Ramon Nario <rtnario@yahoo.com.ph>
2022-05-12 14:31:25 +0800

    Finish exercises 10 to 12

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    const num = +input;
 
+    if (num < 1) return "OOPS";
+    if (num === 1 || num === 2) return 1;
+
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((previousPerson, currentPerson) => {
+        const previousPersonYearOfBirth = previousPerson.yearOfBirth;
+        const currentPersonYearOfBirth = currentPerson.yearOfBirth;
+        let previousPersonYearOfDeath = 0;
+        let currentPersonYearOfDeath = 0;
 
+        if (!previousPerson.hasOwnProperty("yearOfDeath")) {
+            const today = new Date();
+            previousPersonYearOfDeath = today.getFullYear();
+        }
+
+        else {
+            previousPersonYearOfDeath = previousPerson.yearOfDeath;
+        }
+
+        if (!currentPerson.hasOwnProperty("yearOfDeath")) {
+            const today = new Date();
+            currentPersonYearOfDeath = today.getFullYear();
+        }
+
+        else {
+            currentPersonYearOfDeath = currentPerson.yearOfDeath;
+        }
+
+        const previousPersonAge = previousPersonYearOfDeath - previousPersonYearOfBirth;
+        const currentPersonAge = currentPersonYearOfDeath - currentPersonYearOfBirth;
+
+        return previousPersonAge > currentPersonAge ? previousPerson : currentPerson;
+    });
 };
 
 // Do not edit below this line
